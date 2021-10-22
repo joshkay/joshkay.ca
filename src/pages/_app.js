49ae95f26cd3409abb05cc744b22ad733d352ee6
Josh Kay <josh.m.kay@gmail.com>
2021-10-22 15:42:26 -0400

    Moralis broken due to react native & next.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,29 @@
 import { Chakra, getServerSideProps } from 'Chakra';
+import { useEffect } from 'react';
+import { MoralisProvider, useMoralisWeb3Api } from 'react-moralis';
 
 const MyApp = ({ Component, pageProps }) => 
 {
+  const Web3Api = useMoralisWeb3Api();
+  const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(Web3API.account.getNFTs, {
+    
+  });
+
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  console.log(data);
+
   return (
-    <Chakra cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </Chakra>
+    <MoralisProvider
+      appId={process.env.MORALIS_APP_ID}
+      serverUrl={process.env.MORALIS_SERVER_URL}
+    >
+      <Chakra cookies={pageProps.cookies}>
+        <Component {...pageProps} />
+      </Chakra>
+    </MoralisProvider>
   );
 }
 
